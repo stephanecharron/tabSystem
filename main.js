b9ae64f5e65bcf04f6bbdcd8ae0773b63edf4fcd
Stephane Charron <Stephane.Charron@videotron.com>
2017-03-10 17:28:32 -0500

    api start

---
+++ main.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+    angular.module('app').controller('mainController', function(){
+        var vm = this;
+        vm.api = null;
+    });
+})();
--- tabs/tabs.directive.js
+++ tabs/tabs.directive.js
@@ -8,27 +8,56 @@
             templateUrl: 'tabs/tabs.html',
             transclude: true,
             controllerAs: 'tabsCtrl',
-            controller: tabsController
+            controller: tabsController,
+            bindToController : {
+                api: '='
+            }
         };
     }
     
-    function tabsController() {
+    tabsController.$inject = ['Api'];
+    function tabsController(Api) {
         var tabsCtrl = this;
         tabsCtrl.tabs = [];
+        tabsCtrl.tabIndex = 0;
+        var eventRegisterMap = {
+            beforeChange: [],
+            afterChange : []
+        };
+        tabsCtrl.api = new Api(tabsCtrl.tabIndex, eventRegisterMap);
+        var tabCounter = 0;
         
         tabsCtrl.add = function (tab) {
             if (!tabsCtrl.tabs.length) {
                 tab.active = true;
             }
+            tab.index = tabCounter++;
             tabsCtrl.tabs.push(tab);
         };
         
         tabsCtrl.select = function (tab) {
+            tabsCtrl.$$applyCallback('beforeChange');
             angular.forEach(tabsCtrl.tabs, function (tab) {
                 tab.active = false;
             });
             tab.active = true;
+            tabsCtrl.$$applyCallback('afterChange');
+    
         };
+    
+        tabsCtrl.$$applyCallback = function (name) {
+        
+            if(eventRegisterMap[name]){
+                angular.forEach(eventRegisterMap[name], function (callback) {
+                    callback();
+                });
+            } else {
+                throw 'there is no callback name: ' + name;
+            }
+        }
+    
+       
+        
     }
     
 })();
