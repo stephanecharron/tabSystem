0b7b2d3c0ec7f5d086b57c39e6a4dc4c7c2aa2e2
Stephane Charron <Stephane.Charron@videotron.com>
2017-04-04 08:17:37 -0400

    input focus puck

---
+++ input-focus/main.js
@@ -0,0 +1,55 @@
+angular.module('app', []);
+
+angular.module('app').directive('inputsControl', function ($controller) {
+    return {
+        restrict: 'E',
+        template: '<ng-transclude />',
+        controllerAs: 'inputsController',
+        controller: function () {
+            this.inputs =  [{},{},{},{}];
+            var indexInsert = 0;
+            this.pin = '';
+    
+            this.add = function (input) {
+                if(this.inputs.length == 1){
+                    input.focus();
+                }
+                this.inputs[indexInsert].elem = input;
+                this.inputs[0].elem.focus();
+                indexInsert++;
+            };
+            
+            this.next = function (input) {
+                var index = this.inputs.indexOf(input);
+                if(!!this.inputs[index+1]){
+                    this.inputs[index+1].elem.focus();
+                }else {
+                    $('#nf').focus();
+                }
+            };
+            
+            this.update =function () {
+                var _this = this
+                this.pin  ='';
+                     angular.forEach(this.inputs, function (input) {
+                         if(input.value !== null && input.value !== undefined) {
+                             _this.pin += input.value;
+                         }
+                });
+            }
+        },
+        transclude: true
+    }
+});
+
+angular.module('app').directive('pin', function () {
+    return {
+        require: ['^inputsControl', 'pin'],
+        restrict: 'A',
+        controllerAs: 'pinCtrl',
+        controller: function () {},
+        link: function (scope, elem, attrs, ctrl) {
+            ctrl[0].add(elem);
+        }
+    }
+});
